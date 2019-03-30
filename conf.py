7e1361489c2958809b61ef91466f68d714899d58
Junghwan Park <reserve.dev@gmail.com>
2019-03-30 15:14:20 +0900

    Update conf.py
    add trailing slash for site_url

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ version = ''
 # The full version, including alpha/beta/rc tags
 release = ''
 
-site_url = 'https://docs.komoran.kr'
+site_url = 'https://docs.komoran.kr/'
 
 
 # -- General configuration ---------------------------------------------------
